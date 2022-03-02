6e72c5011aa7012a614e208633b65102fd5d7b0b
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2022-03-02 13:52:36 -0700

    last changes

--- src/components/data/projects-data.js
+++ src/components/data/projects-data.js
@@ -9,7 +9,19 @@ export const ProjectData = [
         image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRquEy29kay87rhmCpNtzto2qYUnGnh_tLpdQ&usqp=CAU'
     },
     {
-        id:"1",
+        id:"2",
+        title:"NFT website",
+        demo:'https://chillsburydogeboy.herokuapp.com/',
+        about:"Information website regarding nfts that are being minted on the SOL blockcahin",
+        tags:["React","Node",],
+        github:'https://github.com/PeterdQuinn/ChillsburryMainSite-main.git',
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9w6rYjeT8FJFtQSXPWY8cYqYc6vG0zNP5w&usqp=CAU'
+    },
+
+
+
+    {
+        id:"2",
         title:"Crypto-Now",
         about:"Crytpo-Now is a one stop shop for update information on crytpo currencies and markets",
         tags:["ReactJs","Node","material U-I",],
@@ -17,14 +29,6 @@ export const ProjectData = [
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhj1YnK9BCzkm4Ps2To7b-SlF3L1kcz9UgUQ&usqp=CAU',
       
     },
-    {
-        id:"2",
-        title:"NFT website",
-        demo:'https://chillsburydogeboy.herokuapp.com/',
-        about:"Web page for selling NFTs",
-        tags:["React","Node",],
-        github:'https://github.com/PeterdQuinn/ChillsburryMainSite-main.git',
-        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9w6rYjeT8FJFtQSXPWY8cYqYc6vG0zNP5w&usqp=CAU'
-    },
+   
     
 ];
