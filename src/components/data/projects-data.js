17a495ded47c327deb952dbb0f6214c976dd3253
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2022-03-03 13:56:24 -0700

    Projects 1

--- src/components/data/projects-data.js
+++ src/components/data/projects-data.js
@@ -13,8 +13,7 @@ export const ProjectData = [
         title:"OG Dodeboy NFT",
         demo:'http://www.chillsburydogeboy.com',
         about:"NFT Collection and and information regarding minting date",
-        tags:["React","Node",],
-        github:'https://github.com/PeterdQuinn/ChillsburryMainSite-main.git',
+        tags:["React","Node","Swiper JS",],
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9w6rYjeT8FJFtQSXPWY8cYqYc6vG0zNP5w&usqp=CAU'
     },
 
