2913e9b5ffcb3a3375516c2a29a43f6fa54d4ff3
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2022-03-06 14:00:51 -0700

    changing portfolio

--- src/components/data/projects-data.js
+++ src/components/data/projects-data.js
@@ -1,13 +1,5 @@
 export const ProjectData = [
-    {
-        id:"0",
-        title:"BarterMart",
-        about:"BarterMart is a marketplace to trade items online. Customers can also hold transparent auctions for items with there community",
-        tags:["ReactJs","Node","MongoDB","Express"],
-        demo:'https://bartermart.herokuapp.com/',
-        github:'www.google.com',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRquEy29kay87rhmCpNtzto2qYUnGnh_tLpdQ&usqp=CAU'
-    },
+    
     {
         id:"1",
         title:"OG Dodeboy NFT",
@@ -15,18 +7,6 @@ export const ProjectData = [
         about:"NFT Collection and and information regarding minting date",
         tags:["React","Node","Swiper JS",],
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9w6rYjeT8FJFtQSXPWY8cYqYc6vG0zNP5w&usqp=CAU'
-    },
-
-
-
-    {
-        id:"2",
-        title:"Crypto-Now",
-        about:"Crytpo-Now is a one stop shop for update information on crytpo currencies and markets",
-        tags:["ReactJs","Node","material U-I",],
-        github:'https://github.com/PeterdQuinn/Crypto-For-Beginners',
-        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhj1YnK9BCzkm4Ps2To7b-SlF3L1kcz9UgUQ&usqp=CAU',
-      
     },
    
     
