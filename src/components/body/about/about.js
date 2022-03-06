10a7a595257d835b8b28c25efffe1147a6d428bc
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2022-03-06 12:25:39 -0700

    about

--- src/components/body/about/about.js
+++ src/components/body/about/about.js
@@ -10,7 +10,7 @@ function About() {
                     Hi, My name is 
                     <br /><span className="info-name">Peter Quinn</span>
                     <br />  I am a Full Stack Web Developer of the (MERN) stack. I love Blockchains,NFTs, and crypto currencies.
-                    but I love creating NFTs on Solana!
+                    But I love creating NFTs on Solana!
                 </div>
                 <div className="about-photo">
                     <img src={require('../../../assets/pngegg.png'  )}  width="500" height="333"       className="picture"></img>
