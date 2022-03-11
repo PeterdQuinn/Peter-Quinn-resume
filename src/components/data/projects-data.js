a5c980a7734c63a3dd29e875856f589e1a534da5
Your Name <peterdquinnsr@gmail/com>
2022-03-11 11:58:05 -0700

    4th

--- src/components/data/projects-data.js
+++ src/components/data/projects-data.js
@@ -16,7 +16,7 @@ export const ProjectData = [
         demo:'https://net-cord.herokuapp.com/',
         about:"Real Time chat application",
         tags:["express","Node","jshint","momentjs","inquirer", "socket.io" ],
-        image:'https://imgs.search.brave.com/kN8cb44G-3qu3NJcxhjKkAwC5CEC0t8zU8phOqkSdn0/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/VXRuVTNfb2NvYlE2/R3luZ1Rxc2p3SGFI/YSZwaWQ9QXBp'
+        image:'https://imgs.search.brave.com/dDxyLkA1jSQYNhJ6uNi0Ir_g2n3aIaWwpdP8XfYDV_s/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5i/NGtfaktpakNyRjB0/UTZjcHI5YzJ3SGFI/YSZwaWQ9QXBp'
     },
    
 
@@ -26,7 +26,7 @@ export const ProjectData = [
         demo:'https://your-defi.herokuapp.com/',
         about:"Start up page made for Defi clients",
         tags:["React","Node","axios","React-icons","Coingeko API" ],
-        image:'https://imgs.search.brave.com/dDxyLkA1jSQYNhJ6uNi0Ir_g2n3aIaWwpdP8XfYDV_s/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5i/NGtfaktpakNyRjB0/UTZjcHI5YzJ3SGFI/YSZwaWQ9QXBp'
+        image:'https://imgs.search.brave.com/OvlhY9n5GPqQbXy9-zh7usSePTuDSWr9tcV-c3MrGD0/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5O/dTV6RFFlNFI2cFhE/RVlLSEpPYk1BSGFF/SyZwaWQ9QXBp'
     },
    
 
@@ -36,7 +36,7 @@ export const ProjectData = [
         demo:'',
         about:"",
         tags:["React","Node","Swiper JS",],
-        image:''
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9w6rYjeT8FJFtQSXPWY8cYqYc6vG0zNP5w&usqp=CAU'
     },
    
 
