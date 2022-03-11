be997921c938f3800576800d9184f45d6426c24e
Your Name <peterdquinnsr@gmail/com>
2022-03-11 11:12:55 -0700

    Projects

--- src/components/data/projects-data.js
+++ src/components/data/projects-data.js
@@ -9,5 +9,36 @@ export const ProjectData = [
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9w6rYjeT8FJFtQSXPWY8cYqYc6vG0zNP5w&usqp=CAU'
     },
    
-    
+  
+    {
+        id:"2",
+        title:"Net-Cord",
+        demo:'https://net-cord.herokuapp.com/',
+        about:"Real Time chat application",
+        tags:["express","Node","jshint","momentjs","inquirer", "socket.io" ],
+        image:'https://imgs.search.brave.com/kN8cb44G-3qu3NJcxhjKkAwC5CEC0t8zU8phOqkSdn0/rs:fit:474:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/VXRuVTNfb2NvYlE2/R3luZ1Rxc2p3SGFI/YSZwaWQ9QXBp'
+    },
+   
+
+    {
+        id:"3",
+        title:"Your Defi",
+        demo:'https://your-defi.herokuapp.com/',
+        about:"Start up page made for Defi clients",
+        tags:["React","Node","axios","React-icons","Coingeko API" ],
+        image:'https://imgs.search.brave.com/OvlhY9n5GPqQbXy9-zh7usSePTuDSWr9tcV-c3MrGD0/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5O/dTV6RFFlNFI2cFhE/RVlLSEpPYk1BSGFF/SyZwaWQ9QXBp'
+    },
+   
+
+    {
+        id:"4",
+        title:"OG Dodeboy NFT",
+        demo:'http://www.chillsburydogeboy.com',
+        about:"NFT Collection and and information regarding minting date",
+        tags:["React","Node","Swiper JS",],
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9w6rYjeT8FJFtQSXPWY8cYqYc6vG0zNP5w&usqp=CAU'
+    },
+   
+
+
 ];
