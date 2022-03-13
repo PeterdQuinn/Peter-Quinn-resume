1b4f826b14b9d7e049941e769d4752258c6d47e5
Your Name <peterdquinnsr@gmail/com>
2022-03-13 12:29:11 -0700

    icons

--- src/components/header/mobile/index.js
+++ src/components/header/mobile/index.js
@@ -11,22 +11,22 @@ function Mobile({isOpen, setIsOpen}) {
             <div className="mobile-options">
             <div className="mobile-option">
                 <a href="#projects">
-                <i class="fi fi-rr-layers"></i>Projects
+                <i class="fi fi-brands-windows"></i>Projects
                 </a>
             </div>
             <div className="mobile-option">
                 <a href="#skills">
-                <i class="fi fi-rr-list-check"></i>Skills
+                <i class="fi fi-sr-paint-brush"></i>Skills
                 </a>
             </div>
             <div className="mobile-option">
                 <a href="#work">
-                <i class="fi-rr-briefcase option-icon"></i>Work
+                <i class="fi fi-bs-grid-alt"></i></i>Work
                 </a>
             </div>
             <div className="mobile-option">
                 <a href="#contact">
-                <i class="fi fi-rr-address-book"></i>Contact
+                <i class="fi fi-brands-twitch"></i>Contact
                 </a>
             </div>
             </div>
--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -6,22 +6,22 @@ function Web() {
         <div className="web">
             <div className="web-option">
                 <a href="#projects">
-                <i class="fi fi-rr-layers"></i>Projects
+                <i class="fi fi-rr-list-check"></i>Projects
                 </a>
             </div>
             <div className="web-option">
                 <a href="#skills">
-                <i class="fi fi-rr-list-check"></i>Skills
+                <i class="fi fi-brands-atom"></i>Skills
                 </a>
             </div>
             <div className="web-option">
                 <a href="#work">
-                <i class="fi-rr-briefcase option-icon"></i>Work
+                <i class="fi fi-brands-ethereum"></i>Work
                 </a>
             </div>
             <div className="web-option">
                 <a href="#contact">
-                <i class="fi fi-rr-address-book"></i>Contact
+                <i class="fi fi-brands-at&t"></i>Contact
                 </a>
             </div>
         </div>
