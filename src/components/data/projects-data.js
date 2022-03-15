74313ecd95801deec802ce6596cfa84e3bd564d0
Your Name <peterdquinnsr@gmail/com>
2022-03-14 20:44:03 -0700

    projects del

--- src/components/data/projects-data.js
+++ src/components/data/projects-data.js
@@ -9,19 +9,9 @@ export const ProjectData = [
         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc9w6rYjeT8FJFtQSXPWY8cYqYc6vG0zNP5w&usqp=CAU'
     },
    
-  
+   
     {
         id:"2",
-        title:"Net-Cord",
-        demo:'https://net-cord.herokuapp.com/',
-        about:"Real Time chat application",
-        tags:["express","Node","jshint","momentjs","inquirer", "socket.io" ],
-        image:'https://imgs.search.brave.com/p61e7k53P28JA6n00SOAEiaD8oUFPlM2LRCqeRU6-jI/rs:fit:632:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5w/ZjZpOVprYmFhV1FS/VkJwdkZiSXJRSGFG/aiZwaWQ9QXBp'
-    },
-   
-
-    {
-        id:"3",
         title:"Your Defi",
         demo:'https://your-defi.herokuapp.com/',
         about:"Start up page made for Defi clients",
@@ -30,7 +20,7 @@ export const ProjectData = [
     },
    
     {
-        id:"4",
+        id:"3",
         title:"My-coins",
         demo:'https://my-gold.herokuapp.com/',
         about:"Keep up on your current crypto investments real time",
