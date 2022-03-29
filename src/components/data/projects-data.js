5158f68b1e0a692784e9b94b2f9d8281f051969a
Your Name <peterdquinnsr@gmail/com>
2022-03-28 22:26:53 -0700

    projects

--- src/components/data/projects-data.js
+++ src/components/data/projects-data.js
@@ -28,7 +28,23 @@ export const ProjectData = [
         image:'https://imgs.search.brave.com/BdUvzVJbO3YgShpblryZPASbv4GWA-0UECdgobmJkCQ/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5i/aXhORzRoYWlETEVQ/dTVabDc5V3hRSGFF/NyZwaWQ9QXBp'
     },
    
-
+    {
+        id:"4",
+        title:"My food",
+        demo:'https://chompies.herokuapp.com/',
+        about:"Static website made for resturants",
+        tags:["React","CSS","HTML","Node"],
+        image:'https://www.google.com/imgres?imgurl=https%3A%2F%2Fthumbor.thedailymeal.com%2FMTzCEVrDQvEwrsodZ7hTlL6ML0Y%3D%2F870x565%2Ffilters%3Afocal(980x733%3A981x734)%2Fhttps%3A%2F%2Fwww.thedailymeal.com%2Fsites%2Fdefault%2Ffiles%2Fstory%2F2020%2Fshutterstock_1751836019_0.jpg&imgrefurl=https%3A%2F%2Fwww.thedailymeal.com%2Feat%2Fwhy-restaurant-burgers-best&tbnid=ocT6yZeZwOoNfM&vet=12ahUKEwiQh8qAyur2AhVLADQIHcLnBbQQMygBegUIARDVAQ..i&docid=9cJaqs-EUL0xUM&w=870&h=565&q=restaurant%20pictures%20burgers&hl=en&ved=2ahUKEwiQh8qAyur2AhVLADQIHcLnBbQQMygBegUIARDVAQ'
+    },
+    {
+        id:"5",
+        title:"Future-Travel",
+        demo:'http://future-travel.vercel.app/',
+        about:"Get ready for the future of space travel",
+        tags:["React","Node","React-Router-Dom","React-icons",],
+        image:'https://www.google.com/imgres?imgurl=https%3A%2F%2Fparispeaceforum.org%2Fwp-content%2Fuploads%2F2021%2F10%2FNET-ZERO-SPACE-INITIATIVE-1.png&imgrefurl=https%3A%2F%2Fparispeaceforum.org%2Fen%2Finitiatives%2Fnet-zero-space%2F&tbnid=NA_O-v8xfkJ6hM&vet=12ahUKEwiMmoDZyur2AhUiLDQIHRt3BG8QMygBegUIARDZAQ..i&docid=gPzZP7U1EyP-1M&w=1600&h=900&q=space&hl=en&ved=2ahUKEwiMmoDZyur2AhUiLDQIHRt3BG8QMygBegUIARDZAQ'
+    },
+   
 
 
    
