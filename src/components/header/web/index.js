6d18c34d1d5a43abe1657650b56b7ce02f44b5e5
Your Name <peterdquinnsr@gmail/com>
2022-05-24 16:01:38 -0700

    web

--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -6,7 +6,7 @@ function Web() {
         <div className="web">
             <div className="web-option">
                 <a href="#projects">
-                <i class="fi fi-rr-home"></i>Projects
+                <i class="fi fi-rr-align-justify"></i>Projects
                 </a>
             </div>
             <div className="web-option">
@@ -16,12 +16,12 @@ function Web() {
             </div>
             <div className="web-option">
                 <a href="#work">
-                <i class="fi fi-rr-globe"></i>Work
+                <i class="fi fi-rr-align-justify"></i>Work
                 </a>
             </div>
             <div className="web-option">
                 <a href="#contact">
-                <i class="fi fi-rr-user"></i>Contact
+                <i class="fi fi-rr-align-justify"></i>Contact
                 </a>
             </div>
         </div>
